98023e7131520d820be22250c6ace27f6d6899a5
dmitry <ds@gmail.com>
2022-08-20 19:38:50 +0300

    refactor: removed unused stuff (Select.jsx)

--- src/stories/Select/Select.jsx
+++ src/stories/Select/Select.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect } from "react";
-import { InputLabel, MenuItem, Select as MUISelect, TextField } from "@mui/material";
-import { AE } from "country-flag-icons/react/3x2";
+import {
+  InputLabel,
+  MenuItem,
+  Select as MUISelect,
+  TextField,
+} from "@mui/material";
 
 function Select({ label, rates, onCurrencyHandle, currency, ...props }) {
   const [value, setValue] = React.useState();
+
   const handleChange = (event) => {
-    console.log(event.target.value)
+    console.log(event.target.value);
     setValue(event.target.value);
-    onCurrencyHandle(event.target.value)
-    // console.log(event.target.value)
+    onCurrencyHandle(event.target.value);
   };
 
   return (
@@ -16,7 +20,7 @@ function Select({ label, rates, onCurrencyHandle, currency, ...props }) {
       <TextField
         {...props}
         sx={{
-            width: 250,
+          width: 250,
         }}
         value={value || ""}
         onChange={handleChange}
@@ -24,18 +28,20 @@ function Select({ label, rates, onCurrencyHandle, currency, ...props }) {
         label="Currency"
         helperText="Select a currency you need"
       >
-        {rates ? rates.map( (rate, i) => 
-        <MenuItem
-          key={i}
-          value={rate[1]}
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between'
-          }}
-        >
-          {rate[0]}
-        </MenuItem>): null}
-        
+        {rates
+          ? rates.map((rate, i) => (
+              <MenuItem
+                key={i}
+                value={rate[1]}
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                {rate[0]}
+              </MenuItem>
+            ))
+          : null}
       </TextField>
     </>
   );
